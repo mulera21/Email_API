3d39adc24bfd0e7377822e22a33bcc25e74634a3
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2024-03-18 08:22:57 +0300

    getting data from request

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import requests
 
 api_key = "0807afcb8d864f828d7659fe2fce4720"
 
+# news tesla
 url = ("https://newsapi.org/v2/everything?q=tesla&" 
        "from=2024-02-18&sortBy=published" 
        "At&apiKey=0807afcb8d864f828d7659fe2fce4720")
 
+# make a request
 requests = requests.get(url)
 
-content = requests.text
-print(content)
+# get a dictionary with data
+content = requests.json()
+
+# Access the article  titles and description
+for article in content["articles"]:
+       print(article["title"])
+       print(article["description"])
+
